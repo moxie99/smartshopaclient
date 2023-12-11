e6f2ac181ba4cf758c3845f3a3106d2d10ecb2ab
Adeolu David <adeolusegun99@gmail.com>
2023-12-11 19:59:40 +0100

    added production url

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 const local = 'http://localhost:5000';
 const production = 'https://smartshopabackend.onrender.com';
 const api = axios.create({
-  baseURL: `${local}/api`,
+  baseURL: `${production}/api`,
   withCredentials: true,
 });
 export default api;
